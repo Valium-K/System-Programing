cd179957a07dfdf2add020e4a575546f9cc5a530
Valium-K <lschan96@gmail.com>
2019-10-15 11:37:25 +0900

    1015 수업내용

---
+++ 1015/stcreate2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "student.h"
+#define START_ID 1201001
+
+int main(int argc, char* argv[]) {
+	struct student record;
+	FILE* fp;
+	
+	if(argc != 2) {
+		fprintf(stderr, "사용법: %s 파일이름\n", argv[0]);
+		exit(1);
+	}
+	
+	fp = fopen(argv[1], "wb");
+	
+	printf("%7s %6s %4s\n", "학번", "이름", "점수");
+	
+	while(scanf("%d %s %d", &record.id, record.name, &record.score) == 3) {
+		fseek(fp, (record.id - START_ID)* sizeof(record), SEEK_SET);
+		fwrite(&record, sizeof(record), 1, fp);
+	}
+	
+	fclose(fp);
+	
+	exit(0);
+}
